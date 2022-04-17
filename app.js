68cc72c01b4f46c3848099ea7ebd24b95ded349b
Lucca Araujo <lucca.ara7@gmail.com>
2022-04-16 17:21:22 -0700

    add questions feature

--- app.js
+++ app.js
@@ -42,20 +42,171 @@
 
 
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+
+//const profileDataArgs = process.argv.slice(2);
+//const [name, github] = profileDataArgs;
 //const name = profileDataArgs[0];  older way
 //const github = profileDataArgs[1]; older way
 //assignment destructuring. In basic terms, it assigns elements of an array to variable names in a single expression, as shown here:
 
 
+//project
+const inquirer = require('inquirer');
+//const fs = require('fs');
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+//const generatePage = require('./src/page-template');
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
+
+//const pageHTML = generatePage(name, github);
+
+//fs.writeFile('./index.html', pageHTML, err => {
+//  if (err) throw err;
+
+ // console.log('Portfolio complete! Check out index.html to see the output!');
+//});
+
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Enter your GitHub Username (Required)',
+      validate: githubUserInput => {
+        if (githubUserInput) {
+          return true;
+        } else {
+          console.log('Enter your GitHub Username');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'about',
+      message: 'Provide some information about yourself:'
+    }
+  ]);
+};
+
+const promptProject = portfolioData => {
+  // If there's no 'projects' array property, create one
+if (!portfolioData.projects) {
+  portfolioData.projects = [];
+}
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is the name of your project? (Required)',
+      validate: projectNameInput => {
+        if (projectNameInput) {
+          return true;
+        } else {
+          console.log('What is the name of your project?');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Provide a description of the project (Required)',
+      validate: projectDescriptionInput => {
+        if (projectDescriptionInput) {
+          return true;
+        } else {
+          console.log('Provide a description of the project');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'checkbox',
+      name: 'languages',
+      message: 'What did you build this project with? (Check all that apply)',
+      choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: githubLinkInput => {
+        if (githubLinkInput) {
+          return true;
+        } else {
+          console.log('Enter the GitHub link to your project.');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'confirm',
+      name: 'feature',
+      message: 'Would you like to feature this project?',
+      default: false
+    },
+    {
+      type: 'confirm',
+      name: 'confirmAddProject',
+      message: 'Would you like to enter another project?',
+      default: false
+    }
+  ])
+  
+  .then(projectData => {
+    portfolioData.projects.push(projectData);
+
+    if (projectData.confirmAddProject) {
+      return promptProject(portfolioData);
+
+    } else {
+      return portfolioData;
+    }
+  });
+};
+
+
+
+
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+  console.log(portfolioData);
 });
-
